5c764a6d3f056896c5a69f4ccc9fbaa3b279460a
AusProtoBell <85586146+AusProtoBell@users.noreply.github.com>
2021-08-05 18:37:28 +0300

    task_2 complete

---
+++ lesson_11/task_2.py
@@ -0,0 +1,18 @@
+def palindrome_check(some_string: str) -> bool:
+    data = some_string.lower().\
+        replace(' ', '').replace(',', '').replace('.', '')
+    # Remove all punctuation and spaces for the test
+    return data == data[::-1]
+
+
+print(palindrome_check("А роза упала на лапу Азора."))
+print(palindrome_check("Яро закусала ренегата генерала сука Зоря."))
+print(palindrome_check("Знамо, даже у ежа дома НЗ."))
+print(palindrome_check("Мастер жрет сам."))
+print(palindrome_check("Мастер орет сам."))
+print(palindrome_check("Хил, худ, а дух лих."))
+
+
+print(palindrome_check("Мастъъъер жрет сам"))
+print(palindrome_check("Мастер оръъъет сам"))
+print(palindrome_check("Хил, худ, а дух лиъъъх"))
