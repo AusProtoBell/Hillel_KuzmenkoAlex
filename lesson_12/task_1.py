0b2d0b8daab32b15398faf4605760d3251300c2e
AusProtoBell <85586146+AusProtoBell@users.noreply.github.com>
2021-08-11 00:41:13 +0300

    added class bank + reworking all code

--- lesson_12/task_1.py
+++ lesson_12/task_1.py
@@ -2,6 +2,16 @@ from uuid import uuid4
 import datetime
 
 
+class UaBank:
+    def __init__(self, bank_name: str, capital: float, commission: float):
+        self.bank_name = bank_name
+        self.capital = capital
+        self.commission = commission
+
+    def balance_check(self):
+        return self.capital
+
+
 class BankID:
 
     activity_list = []
@@ -12,27 +22,38 @@ class BankID:
         self.balance = balance
 
     def deposit(self, value):
-        self.balance += 0.99 * value
-        self.activity_list.append([value * 0.99, 'deposit', datetime.datetime.now().strftime('%Y-%m-%d %I:%M%p')])
+        self.balance += value
+        self.activity_list.append([value, 'deposit', datetime.datetime.now().strftime('%Y-%m-%d %I:%M%p')])
 
     def withdrawal_funds(self, value):
-        if value * 1.01 > self.balance:
+        if value> self.balance:
             raise Exception("Insufficient funds")
-        self.balance -= 1.01 * value
-        self.activity_list.append([f'-{value * 1.01}', 'withdrawal',
-                                   datetime.datetime.now().strftime('%Y-%m-%d %I:%M%p')])
+        self.balance -= value
+        self.activity_list.append([f'-{value}', 'withdrawal', datetime.datetime.now().strftime('%Y-%m-%d %I:%M%p')])
 
     def balance_check(self):
         return self.balance
 
 
+def bank_transaction(user, bank, transaction_type, value):
+    if transaction_type == 'deposit':
+        user.deposit(value * (1 - bank.commission))
+    elif transaction_type == 'withdrawal':
+        user.withdrawal_funds(value * (1 + bank.commission))
+    else:
+        raise Exception('Incorrect transaction type')
+    bank.capital += value * bank.commission
+
+
 if __name__ == '__main__':
 
     user_one = BankID('Dave', 10000)
+    bank_one = UaBank('SmileBank', 1000000, 0.01)
 
-    user_one.deposit(5000)
-    user_one.withdrawal_funds(3500)
+    bank_transaction(user_one, bank_one, 'deposit', 5000)
+    bank_transaction(user_one, bank_one, 'withdrawal', 3500)
 
     print(user_one.user_id)
     print(user_one.balance_check())
     print(user_one.activity_list)
+    print(bank_one.capital)
