a180ddf219bf6567033c43a0936874bad185c23d
AusProtoBell <85586146+AusProtoBell@users.noreply.github.com>
2021-08-11 21:16:35 +0300

    rollback to stable version

--- lesson_12/task_1.py
+++ lesson_12/task_1.py
@@ -2,58 +2,38 @@ from uuid import uuid4
 import datetime
 
 
-class UaBank:
-    def __init__(self, bank_name: str, capital: float, commission: float):
-        self.bank_name = bank_name
-        self.capital = capital
-        self.commission = commission
-
-    def balance_check(self):
-        return self.capital
-
-
 class BankID:
 
-    activity_list = []
+    bank_commission = 0.01
+    # x% = x/100
 
     def __init__(self, user_name: str, balance: float):
         self.user_id = uuid4()
         self.user_name = user_name
         self.balance = balance
+        self.activity_list = []
 
     def deposit(self, value):
-        self.balance += value
-        self.activity_list.append([value, 'deposit', datetime.datetime.now().strftime('%Y-%m-%d %I:%M%p')])
+        new_value = value * (1 - self.bank_commission)
+        self.balance += new_value
+        self.activity_list.append([new_value, 'deposit', datetime.datetime.now().strftime('%Y-%m-%d %I:%M%p')])
 
     def withdrawal_funds(self, value):
-        if value> self.balance:
+        new_value = value * (1 + self.bank_commission)
+        if new_value > self.balance:
             raise Exception("Insufficient funds")
-        self.balance -= value
-        self.activity_list.append([f'-{value}', 'withdrawal', datetime.datetime.now().strftime('%Y-%m-%d %I:%M%p')])
+        self.balance -= new_value
+        self.activity_list.append([f'-{new_value}', 'withdrawal', datetime.datetime.now().strftime('%Y-%m-%d %I:%M%p')])
 
     def balance_check(self):
         return self.balance
 
 
-def bank_transaction(user, bank, transaction_type, value):
-    if transaction_type == 'deposit':
-        user.deposit(value * (1 - bank.commission))
-    elif transaction_type == 'withdrawal':
-        user.withdrawal_funds(value * (1 + bank.commission))
-    else:
-        raise Exception('Incorrect transaction type')
-    bank.capital += value * bank.commission
-
-
 if __name__ == '__main__':
 
-    user_one = BankID('Dave', 10000)
-    bank_one = UaBank('SmileBank', 1000000, 0.01)
+    client_one = BankID("Dave", 10000)
+    client_one.deposit(5000)
+    client_one.withdrawal_funds(3500)
 
-    bank_transaction(user_one, bank_one, 'deposit', 5000)
-    bank_transaction(user_one, bank_one, 'withdrawal', 3500)
-
-    print(user_one.user_id)
-    print(user_one.balance_check())
-    print(user_one.activity_list)
-    print(bank_one.capital)
+    print(client_one.balance_check())
+    print(client_one.activity_list)
